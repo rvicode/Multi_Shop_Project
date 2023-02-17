9cbf71c41f1821f384e499013f4a2ef9291de5bc
Arvin Visi <arvinvisi03@gmail.com>
2023-02-17 11:02:49 +0330

    add Product and translate models

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name_product', 'short_description', 'description', 'price']
+    search_fields = ['name_product', 'short_description', 'description']
--- product/models.py
+++ product/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Product(models.Model):
+    scores = (
+        ('bad', 1),
+        ('not bad', 2),
+        ('good', 3),
+        ('great', 4),
+        ('perfect', 5)
+    )
+    name_product = models.CharField(max_length=100, verbose_name=_("Name Product"))
+    short_description = models.TextField(verbose_name=_("Short Description"))
+    description = models.TextField(verbose_name=_("Description"))
+    price = models.PositiveIntegerField(verbose_name=_("Price"))
+    score = models.CharField(max_length=20, choices=scores)
+
+    def __str__(self):
+        return self.name_product
+
+    class Meta:
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
