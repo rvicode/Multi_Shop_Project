dcedf48837952ef6d65cfa14ed8cf5c7ead5a9c5
Arvin Visi <arvinvisi03@gmail.com>
2023-02-20 10:45:36 +0330

    add comment in product_detail

--- product/views.py
+++ product/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Product
+from .models import Product, Comment
 
 
 def product_detail_view(request, pk):
+
     product = get_object_or_404(Product, id=pk)
     product_list = Product.objects.all()
-    return render(request, 'product/product_detail.html', {'product': product, 'product_list': product_list})
+
+    return render(request, 'product/product_detail.html',
+                  {'product': product, 'product_list': product_list})
