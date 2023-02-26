794d6a216a3e81f878fbcd7681b23c5063db346c
Arvin Visi <arvinvisi03@gmail.com>
2023-02-26 12:19:15 +0330

    add forms and cart_detail_view

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
 
-# Create your views here.
+from .cart import Cart
+from .forms import ProductCartForm
+
+
+@require_GET
+def cart_detail_view(request):
+    cart = Cart(request)
+
+    for item in cart:
+        item['product_update_quantity_form'] = ProductCartForm(initial={
+            'quantity': item['quantity'],
+            'inplace': True,
+        })
+
+    return render(request, 'cart/cart_detail.html', {'cart': cart})
