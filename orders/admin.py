55db7ae702a18113b70c4b986996c7847ad4c94a
Arvin Visi <arvinvisi03@gmail.com>
2023-03-03 17:33:18 +0330

    add CommentInline and ProductAdmin and CommentAdmin in admin file

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+
+class CommentsInline(admin.TabularInline):
+    model = OrderItem
+    fields = ['order', 'product', 'quantity', 'price']
+    extra = 1
+
+
+@admin.register(Order)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['username', 'address', 'is_paid', 'datetime_create']
+    inlines = [
+        CommentsInline,
+    ]
+
+
+@admin.register(OrderItem)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'quantity', 'price', 'datetime_create']
