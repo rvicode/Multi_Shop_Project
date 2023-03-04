0f931ce92ff1cd5bbb096452118d5558730c97c8
Arvin Visi <arvinvisi03@gmail.com>
2023-03-04 10:23:09 +0330

    add payment_callback

--- payment/urls.py
+++ payment/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('process/', views.payment_process, name='payment_process'),
+    path('callback/', views.payment_callback, name='payment_callback'),
 ]
--- payment/views.py
+++ payment/views.py
@@ -39,3 +39,52 @@ def payment_process(request):
         return redirect(f'https://www.zarinpal.com/pg/StartPay/{authority}')
     else:
         return HttpResponse('Zarinpal Error')
+
+
+def payment_callback(request):
+    payment_authority = request.GET.get('Authority')
+    payment_status = request.GET.get('Status')
+
+    order = get_object_or_404(Order, zarin_authority=payment_authority)
+    toman_total_price = order.get_total_price()
+    rial_total_price = toman_total_price * 10
+
+    if payment_status == 'OK':
+        request_header = {
+            "accept": "application/json",
+            "content-type": "application/json",
+        }
+
+        request_data = {
+            'merchant_id': settings.ZARINPAL_MERCHANT_ID,
+            'amount': rial_total_price,
+            'authority': payment_authority,
+        }
+
+        res = requests.post(url='https://api.zarinpal.com/pg/v4/payment/verify.json', data=json.dumps(request_data),
+                            headers=request_header)
+
+        if 'data' in res.json() and ('errors' not in res.json()['data'] or len(res.json()['errors'] == 0)):
+            data = res.json()['data']
+            payment_code = data['code']
+
+            if payment_code == 100:
+                order.is_paid = True
+                order.zarin_ref_id = data['ref_id']
+                order.zarin_data = data
+                order.save()
+                return redirect('product:product_list')
+
+            elif payment_code == 101:
+                return redirect('product:product_list')
+
+            else:
+                payment_error = res.json()['errors']['code']
+                payment_message = res.json()['errors']['message']
+                return redirect('product:product_list')
+
+        else:
+            return redirect('product:product_list')
+
+    else:
+        return redirect('product:product_list')
